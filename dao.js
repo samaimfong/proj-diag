efc3f1b5793c7158933487c4804fbc08b672aca4
darry <darrychui@msn.com>
2020-02-13 21:51:13 -0800

    modified dao.js to use promise

--- dao.js
+++ dao.js
@@ -1,64 +1,69 @@
-const sqlite3 = require('sqlite3').verbose()
-const Promise = require('./node_modules/bluebird')
+const sqlite3 = require("sqlite3").verbose();
+// const Promise = require('./node_modules/bluebird')
 
 class AppDAO {
-    // constructor
-    constructor(dbFilePath) {
-        this.db = new sqlite3.Database(dbFilePath, (err) => {
-        if (err) {
-            console.log('Could not connect to database', err)
-        } else {
-            console.log('Connected to database')
-        }
-        })
-    }
+  // constructor
 
-    // run method
-    run(sql, params = []) {
-        return new Promise((resolve, reject) => {
-        this.db.run(sql, params, function (err) {
-            if (err) {
-            console.log('Error running sql ' + sql)
-            console.log(err)
-            reject(err)
-            } else {
-            resolve({ id: this.lastID })
-            }
-        })
-        })
-    }
-    get(sql, params = []) {
-        return new Promise((resolve, reject) => {
-        this.db.get(sql, params, (err, result) => {
-            if (err) {
-            console.log('Error running sql: ' + sql)
-            console.log(err)
-            reject(err)
-            } else {
-            resolve(result)
-            }
-        })
-        })
-    }
-
-    all(sql, params = []) {
+  // connect
+  connect(dbFilePath) {
     return new Promise((resolve, reject) => {
-        this.db.all(sql, params, (err, rows) => {
+      this.db = new sqlite3.Database(dbFilePath, err => {
         if (err) {
-            console.log('Error running sql: ' + sql)
-            console.log(err)
-            reject(err)
+          reject(new Error(err.message));
+          // console.log('Could not connect to database', err)
         } else {
-            resolve(rows)
+          resolve("Connected to database");
+          // console.log('Connected to database')
         }
-        })
-    })
-    }
+      });
+    });
+  }
 
-    close(){
-        this.db.close()
-    }
+  // run method
+  run(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.run(sql, params, function(err) {
+        if (err) {
+          console.log("Error running sql " + sql);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve({ id: this.lastID });
+        }
+      });
+    });
+  }
+  get(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.get(sql, params, (err, result) => {
+        if (err) {
+          console.log("Error running sql: " + sql);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 
+  all(sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, rows) => {
+        if (err) {
+          console.log("Error running sql: " + sql);
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  close() {
+    this.db.close();
+  }
 }
 
-module.exports = AppDAO
+module.exports = AppDAO;
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-import Promise from 'bluebird'
-const AppDAO = require('./dao')
-const StateRepository = require('./StateTouches_repository')
+// import Promise from "bluebird";
+const AppDAO = require("./dao");
+//const StateRepository = require("./StateTouches_repository");
 
 function main() {
-  const dao = new AppDAO(':memory:')
+  console.log("Enter Main");
+  const dao = new AppDAO();
+  dao
+    .connect(":memory:")
+    // .connect()
+    .then(result => console.log("result of connect:", result))
+    .catch(err => console.log("error : ", err.message));
+  /*   const dao = new AppDAO(':memory:')
   const s = new StateRepository(dao)
   let projectid
 
@@ -16,25 +23,10 @@ function main() {
 
 
   )
-
+ */
 }
 
-main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main();
 
 // const sqlite3 = require('sqlite3').verbose();
 
@@ -58,13 +50,12 @@ main()
 //         }
 //         // console.log(`${row.Id} ${row.State} ${row.StateTouches}`);
 //         console.log(row);
-//     });    
+//     });
 //   }
 //   );
-  //db.run('Create Index idx_StateTouches_State on StateTouches (State);');
-  //db.run('Create Index idx_StateTouches_StateTouches on StateTouches (StateTouches);');
+//db.run('Create Index idx_StateTouches_State on StateTouches (State);');
+//db.run('Create Index idx_StateTouches_StateTouches on StateTouches (StateTouches);');
 // db.run('.mode csv StateTouches');
 // db.run('.import /home/darry/proj-diag/data/statetouches.csv StateTouches');
-
 
 // db.close();
